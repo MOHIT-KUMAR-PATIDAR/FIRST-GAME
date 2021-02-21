df8a10355b10d0b312789295c088cc0aa055e1ef
MOHIT-KUMAR-PATIDAR <76639143+MOHIT-KUMAR-PATIDAR@users.noreply.github.com>
2021-02-21 18:59:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var randomnumber1 = Math.floor(Math.random() *6) + 1;
+
+var randomDiceImage = "dice" + randomnumber1 + ".png";
+var randomImageSource ="images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+var randomnumber2 = Math.floor(Math.random() *6) + 1;
+
+var randomDiceImage2 = "dice" + randomnumber2 + ".png";
+var randomImageSource2 ="images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
